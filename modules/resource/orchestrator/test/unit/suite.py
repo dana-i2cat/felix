110b55fa39f3de6f2643fc3ba041c3c24f36c231
Carolina Fernandez <carolina.fernandez@i2cat.net>
2014-04-29 19:03:08 +0200

    Unit test: added basic test for ListResources     > Temporarily: credentials for 'alice' distributed until GID auto-generated

--- modules/resource/orchestrator/test/unit/suite.py
+++ modules/resource/orchestrator/test/unit/suite.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python
 
 import unittest2 as unittest
+import sys
+sys.path.insert(0, "../../src")
 
-from geni.v3.computing_rm import TestComputingRM
+from geni.v3.test_api import TestGENIv3API
 
 def testing_suite():
     """
     Explicitely add unit tests to the suite.
     """
     suite = unittest.TestSuite()
-    suite.addTest(TestComputingRM())
+    suite.addTest(TestGENIv3API())
     return suite
 
 if __name__ == '__main__':
