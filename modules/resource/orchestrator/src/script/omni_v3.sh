6e8462d4c87e93d820fd4cfe52d3271c877eb626
Carolina Fernandez <carolina.fernandez@i2cat.net>
2014-04-29 16:53:32 +0200

    Improving 'script' reference to launch GENI CH, main server and some omni commands (II)

--- modules/resource/orchestrator/src/script/omni_v3.sh
+++ modules/resource/orchestrator/src/script/omni_v3.sh
@@ -10,14 +10,14 @@ echo "GENI. Starting Clearinghouse"
 echo ".............................."
 python $gcf_path/src/gcf-ch.py &
 
-sleep 2
+sleep .5
 
 echo ".............................."
 echo "RO. Starting main server"
 echo ".............................."
 python ../main.py &
 
-sleep 2
+sleep .5
 
 echo ".............................."
 echo "GENI v3. GetVersion"
@@ -32,5 +32,7 @@ python $gcf_path/src/omni.py -o -a https://localhost:8440/geni/3 -V 3 --debug --
 echo ".............................."
 echo "Killing server processes"
 echo ".............................."
-ps axf | grep "main.py" | grep "python.py" | grep -v grep | awk '{print "kill -9 " $1}'
-ps axf | grep "gcf-ch.py" | grep "python.py" | grep -v grep | awk '{print "kill -9 " $1}'
+#ps axf | grep "main.py" | grep "python" | grep -v grep | awk '{print "kill -9 " $1}'
+kill $(ps aux | grep "main.py" | grep "python" | grep -v grep | awk '{print $2}')
+#ps axf | grep "gcf-ch.py" | grep "python" | grep -v grep | awk '{print "kill -9 " $1}'
+kill $(ps aux | grep "gcf-ch.py" | grep "python" | grep -v grep | awk '{print $2}')
