5b55a08af37c2c85f1ea1d2db2c6c0c1ef4f7658
Carolina Fernandez <carolina.fernandez@i2cat.net>
2014-04-29 16:41:13 +0200

    Improving 'script' reference to launch GENI CH, main server and some omni commands

--- modules/resource/orchestrator/src/script/omni_v3.sh
+++ modules/resource/orchestrator/src/script/omni_v3.sh
@@ -3,6 +3,22 @@
 # Set your GCF path here
 gcf_path="/opt/geni/gcf-2.4"
 
+## Two first commands should be run manually
+
+echo ".............................."
+echo "GENI. Starting Clearinghouse"
+echo ".............................."
+python $gcf_path/src/gcf-ch.py &
+
+sleep 2
+
+echo ".............................."
+echo "RO. Starting main server"
+echo ".............................."
+python ../main.py &
+
+sleep 2
+
 echo ".............................."
 echo "GENI v3. GetVersion"
 echo ".............................."
@@ -12,3 +28,9 @@ echo ".............................."
 echo "GENI v3. ListResources"
 echo ".............................."
 python $gcf_path/src/omni.py -o -a https://localhost:8440/geni/3 -V 3 --debug --no-compress listresources
+
+echo ".............................."
+echo "Killing server processes"
+echo ".............................."
+ps axf | grep "main.py" | grep "python.py" | grep -v grep | awk '{print "kill -9 " $1}'
+ps axf | grep "gcf-ch.py" | grep "python.py" | grep -v grep | awk '{print "kill -9 " $1}'
