f7bea1d01952d6e87b50c3e404a7657da3cc17f4
Carolina Fernandez <carolina.fernandez@i2cat.net>
2014-04-29 19:23:09 +0200

    Main server: replacing 'print' for 'logger'

--- modules/resource/orchestrator/src/main.py
+++ modules/resource/orchestrator/src/main.py
@@ -1,5 +1,8 @@
 import sys
 
+from core import log
+logger=log.getLogger("mainserver")
+
 from handler.geni.v3.handler_v3 import GENIv3Handler
 from delegate.geni.v3.delegate_v3 import GENIv3Delegate
 
@@ -11,27 +14,23 @@ def main(argv=None):
     if not argv:
         argv = sys.argv
 
+    # Try to handle unexpected exceptions
     try:
         # Create and register the RPC server
         flaskserver = FlaskServer()
         xmlrpc = FlaskXMLRPC(flaskserver)
-
         # GENIv3
         geni_v3_handler = GENIv3Handler()
         geni_v3_delegate = GENIv3Delegate()
         geni_v3_handler.setDelegate(geni_v3_delegate)
         xmlrpc.registerXMLRPC("geni3_ro", geni_v3_handler, "/geni/3")
-
         # Run server
         flaskserver.runServer()
-
     except KeyboardInterrupt:
         return True
-
-    except Exception as ex:
-        print >>sys.stderr, "Got an exception: " + str(ex)
+    except Exception as e:
+        logger.error("Got an exception: %s" % str(e))
         return False
-
     return True
 
 
