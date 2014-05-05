661c1b6e11686b179bce565639bceb8c44bf83b8
Carolina Fernandez <carolina.fernandez@i2cat.net>
2014-05-05 15:13:38 +0200

    Deploy: written commands to add CRM and SDN entries

--- modules/resource/orchestrator/deploy/install.sh
+++ modules/resource/orchestrator/deploy/install.sh
@@ -7,6 +7,7 @@ echo "Installing Resource Orchestrator..."
 
 # Configuring database with a dummy entry
 dbmanage_path="../src/admin/db"
-python $dbmanage_path/manage.py add_route_entry -t "virtualisation" -a 127.0.0.1 -p 8445 --protocol "https" --endpoint "xmlrpc/plugin/" --user "dummy" --password "dummy"
+python $dbmanage_path/manage.py add_route_entry -t "virtualisation" -a 127.0.0.1 -p 8445 --protocol "https" --endpoint "xmlrpc/sfa/" --user "dummy" --password "dummy"
+python $dbmanage_path/manage.py add_route_entry -t "sdn_networking" -a 127.0.0.1 -p 8443 --protocol "https" --endpoint "xmlrpc/sfa/" --user "dummy" --password "dummy"
 
 echo "Installing Resource Orchestrator... Done"
