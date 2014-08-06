a49754b3c6c403b149fa5e602062114a5d1553e3
Roberto Monno <r.monno@nextworks.it>
2014-08-06 10:52:34 +0200

    little changes

--- modules/resource/orchestrator/src/delegate/geni/v3/detector/resource_detector.py
+++ modules/resource/orchestrator/src/delegate/geni/v3/detector/resource_detector.py
@@ -11,13 +11,13 @@ class ResourceDetector(Service):
     This object can be used to populate the internal RO's DB
     with the available resources exposed by RMs.
     """
-    
+
     def __init__(self, interval=30):
         self.config = ConfParser("ro.conf")
         self.interval = ast.literal_eval(
-                    self.config.get("service \"resource_detector\"").get("interval")
-                    )
-        super(ResourceDetector, self).__init__("ResourceDetector", self.interval)
+            self.config.get("service \"resource_detector\"").get("interval"))
+        super(ResourceDetector, self).__init__(
+            "ResourceDetector", self.interval)
 
     def info(self, msg):
         logger.info("[%s] %s" % (self.name, msg,))
