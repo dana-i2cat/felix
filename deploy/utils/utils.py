bab6857a71f582eeb6f4a4a3c3d669f67b6a95df
Carolina Fernandez <carolina.fernandez@i2cat.net>
2014-10-06 18:03:00 +0200

    Deploy: ignoring new folders in FELIX

--- deploy/utils/utils.py
+++ deploy/utils/utils.py
@@ -24,9 +24,10 @@ def print_error(text):
 ## Paths and files
 
 def get_modules():
+    unallowed_folders = [".git", "deploy", "doc", "modules"]
     ocf_modules = sorted(os.walk(ocf_path).next()[1])
-    ocf_modules.remove("deploy")
-    ocf_modules.remove(".git")
+    for unallowed_folder in unallowed_folders:
+        ocf_modules.remove(unallowed_folder)
     return ocf_modules
 
 def get_installed_modules():
