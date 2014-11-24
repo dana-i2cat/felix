2937e056edea05f68bfcfe544284bed76614dbe2
Carolina Fernandez <carolina.fernandez@i2cat.net>
2014-11-24 12:48:39 +0100

    Adding initial deployment info on README.md

--- modules/resource/orchestrator/deploy/install.sh
+++ modules/resource/orchestrator/deploy/install.sh
@@ -20,8 +20,7 @@ db_content=$(python $dbmanage_path/manage.py dump | wc -l)
 db_lines_to_show=$(($db_content-2))
 if [[ $(python $dbmanage_path/manage.py dump | tail -$db_lines_to_show) == "" ]]; then
   echo "Filling database..."
-  python $dbmanage_path/manage.py add_route_entry -t "virtualisation" -a 127.0.0.1 -p 8445 --protocol "https" --endpoint "xmlrpc/sfa/" --user "dummy" --password "dummy"
-  python $dbmanage_path/manage.py add_route_entry -t "sdn_networking" -a 127.0.0.1 -p 8443 --protocol "https" --endpoint "xmlrpc/sfa/" --user "dummy" --password "dummy"
+  python $dbmanage_path/dummy_install.sh
   echo "Filling database... Done"
 fi
 
