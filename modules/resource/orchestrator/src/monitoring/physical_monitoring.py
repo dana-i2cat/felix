7064b34f69a51c01d10551f1edcb996886a0990e
Carolina Fernandez <carolina.fernandez@i2cat.net>
2014-12-09 19:48:17 +0100

    Physical monitoring: basic structure, part I

--- modules/resource/orchestrator/src/monitoring/physical_monitoring.py
+++ modules/resource/orchestrator/src/monitoring/physical_monitoring.py
@@ -1,7 +1,9 @@
 from core.config import ConfParser
 from delegate.geni.v3.db_manager import db_sync_manager
+from lxml import etree
 
 import core
+import time
 import xml.etree.ElementTree as ET
 
 logger = core.log.getLogger("monitoring-manager")
@@ -13,15 +15,15 @@ class PhysicalMonitoring():
     """
 
     def __init__(self):
-        self.peers = db_sync_manager.get_configured_peers()
-        self.topology = ET.Element("topology")
+        self.peers = [p for p in db_sync_manager.get_configured_peers()]
+        self.topology_list = ET.Element("topology_list")
         self.retrieve_topology()
 
     def retrieve_topology(self):
-        self.debug("Configured peers=%d" % (len(self.peers)))
+        logger.debug("Configured peers=%d" % (len(self.peers)))
         for peer in self.peers:
-            self.debug("Peer=%s" % (peer,))
-            # General information
+            logger.debug("Peer=%s" % (peer,))
+            # General information (
             self.__add_general_info()
             # COM resources
             self.__add_com_info()
@@ -31,74 +33,148 @@ class PhysicalMonitoring():
             self.__add_tn_info()
             # SE resources
             self.__add_se_info()
+        logger.debug("Resulting RSpec=%s" % self.get_topology_pretty())
+
+    ##################
+    # Return topology
+    ##################
 
     def get_topology_tree(self):
-        return self.topology
+        # Return whole list of topologies
+        return self.topology_list
 
     def get_topology(self):
         return ET.tostring(self.get_topology_tree())
 
-    def __add_general_info(self):
-        self.topology.attrib["type"] = "physical"
-        self.topology.attrib["last_update_time"] = "" # TODO Fill
-        for peer in self.peers:
-            n = ET.SubElement(self.topology, "network")
-            n.attrib["name"] = db_sync_manager.get_domain_id()
+    def get_topology_pretty(self):
+        # XML not in proper format: need to convert to lxml, then pretty-print
+        return etree.tostring(etree.fromstring(self.get_topology()), pretty_print=True)
 
-    def __add_com_info(self, root):
-        # 1. Nodes
-        nodes = db_sync_manager.get_com_nodes()
-        for node in nodes:
-            self.debug("com-nodes=%s" % (node,))
-            n = ET.SubElement(root, "node")
+    ##########
+    # Helpers
+    ##########
+
+    def __get_timestamp(self):
+        # Return integer part as a string
+        return str(int(time.time()))
+
+    def __add_general_info(self):
+        topo = ET.SubElement(self.topology_list, "topology")
+        # Milliseconds in UTC format
+        topo.attrib["last_update_time"] = self.__get_timestamp()
+        topo.attrib["type"] = "physical"
+        # TODO Retrieve from config OR from slice!
+        # TODO Filter topology based on this!
+        topo.attrib["name"] = "urn_of_domain"
+        # Set topology tag as root node for subsequent operations
+        self.topology = topo
+
+    def __add_generic_node(self, parent_tag, node, node_type):
+        n = ET.SubElement(parent_tag, "node") 
+        # TODO Fill node ID as appropriate (check if URN is correct, etc)
+        if node_type == "server":
             n.attrib["id"] = node.get("component_id")
-            # TODO Import urn_to_hrn, then extract authority
-            n.attrib["network_name"] = node.get("component_manager_id")
-            n.attrib["type"] = "server"
-            #display = ET.SubElement(n, "display_name")
-            #display.text = node.get("component_manager_id")
-            # TODO Set table for VirtualMachines per server
-            #server_info = ET.SubElement(n, "node")
-            #for vm in n.get("virtual_machines"):
-            #    v = ET.SubElement(server_info, "vm_id")
-            #    v.text = vm.get("component_id")
-            ## Output interfaces per server
-            for iface in n.get("interfaces"):
+        elif node_type == "switch":
+            n.attrib["id"] = node.get("dpid")
+        n.attrib["type"] = node_type
+        # Generate management section for node
+        n = self.__add_management_section(n)
+        return n
+
+    def __add_management_section(self, parent_node):
+        management = ET.SubElement(parent_node, "management")
+        # TODO Query database for management information per resource
+        # TODO Fill with retrieved information as needed
+        # TODO Identify and update must/optional tags/attributes
+#        resource_management_info = db_sync_manager.get_management_info(
+#                                        component_id=parent_node.get("component_id"))
+        management.attrib["type"] = "snmp"
+        address = ET.SubElement(management, "address")
+        address.text = "102.168.2.1"
+        port = ET.SubElement(management, "port")
+        port.text = "161"
+        auth_id = ET.SubElement(management, "auth_id")
+        auth_id.text = "public"
+        auth_pass = ET.SubElement(management, "auth_pass")
+        auth_pass.text = ""
+        return parent_node
+
+    def __add_generic_link(self, link):
+        logger.debug("com-links=%s" % (link,))
+        l = ET.SubElement(self.topology, "link")
+        l.attrib["type"] = link.get("link_type", "lan")
+        # TODO Change structure of data
+        links = link.get("links")
+        for link_i in links:
+            # XXX May need to parse, leave only the switch DPID and/or server
+            # Source
+            iface = ET.SubElement(l, "interface_ref")
+            iface.attrib["client_id"] = link_i.get("source_id")
+            # Destination
+            iface = ET.SubElement(l, "interface_ref")
+            iface.attrib["client_id"] = link_i.get("dest_id")
+
+    #################
+    # C-RM resources
+    #################
+
+    def __add_com_info(self):
+        # 1. Nodes
+        # TODO Filter nodes by network/domain
+        nodes = [ n for n in db_sync_manager.get_com_nodes() ]
+        for node in nodes:
+            logger.debug("com-node=%s" % (node,))
+            n = self.__add_generic_node(self.topology, node, "server")
+            # Output interfaces per server
+            logger.debug("com-node-interfaces=%s" % node.get("interfaces"))
+            for iface in node.get("interfaces"):
                 interface = ET.SubElement(n, "interface")
-                interface.attrib["id"] = "%s_%s" % (n.get("component_id"), iface)
+                # TODO Parse attribute "component_id" to be the expected one
+                interface.attrib["id"] = "%s+eth+%s" % (n.attrib["id"], iface)
 
         # 2. Links
-        links = db_sync_manager.get_com_links()
+        # TODO Filter links by network/domain
+        links = [ l for l in db_sync_manager.get_com_links() ]
+        logger.debug("com-links=%s" % (links,))
         for link in links:
             self.__add_generic_link(link)
 
-    def __add_generic_link(link):
-        self.debug("com-links=%s" % (link,))
-        l = ET.SubElement(self.topology, "link")
-        l.attrib["type"] = link.get("link_type")
-        # TODO Change structure of data
-        prop = link.get("links")
-        for p in prop:
-            iface = ET.SubElement(l, "interface_ref")
-            # XXX May need to parse, leave only the switch DPID and/or server
-            iface.attrib["client_id"] = l.get("component_id")
-            l.attrib["network_name"] = l.get("component_id")
+    ###################
+    # SDN-RM resources
+    ###################
 
-    def __add_sdn_info(self, root):
-        datapaths = db_sync_manager.get_sdn_datapaths()
+    def __add_sdn_info(self):
+        # TODO Filter datapaths by network/domain
+        datapaths = [ d for d in db_sync_manager.get_sdn_datapaths() ]
         for dp in datapaths:
-            self.debug("sdn-datapath=%s" % (dp,))
-            switch = ET.SubElement(root, "switch")
-            switch.attrib["id"] = dp.get("dpid")
+            logger.debug("sdn-datapath=%s" % (dp,))
+            switch = self.__add_generic_node(self.topology, dp, "switch")
             for p in dp.get("ports"):
-                intf = ET.SubElement(switch, "interface")
-                intf.attrib["id"] = p.get("name")
-                intf.attrib["number"] = p.get("num")
+                iface = ET.SubElement(switch, "interface")
+                #iface.attrib["id"] = p.get("name")
+                iface.attrib["id"] = "%s_%s" % (switch.attrib["id"], p.get("num"))
+                port = ET.SubElement(iface, "port")
+                port.attrib["num"] = p.get("num")
 
-        (sdn_links, fed_links) = db_sync_manager.get_sdn_links()
+        # TODO Filter links by network/domain
+        (sdn_links, fed_links) = [ l for l in db_sync_manager.get_sdn_links() ]
         for sdnl in sdn_links:
-            self.debug("sdn-link=%s" % (sdnl,))
+            logger.debug("sdn-link=%s" % (sdnl,))
 
         for fedl in fed_links:
-            self.debug("fed-link=%s" % (fedl,))
+            logger.debug("fed-link=%s" % (fedl,))
+
+    ##################
+    # TN-RM resources
+    ##################
+
+    def __add_tn_info(self):
+        pass
+
+    ##################
+    # SE-RM resources
+    ##################
+
+    def __add_se_info(self):
+        pass
 
