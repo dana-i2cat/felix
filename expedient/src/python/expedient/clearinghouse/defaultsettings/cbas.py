9e00991abde493ece5f0995042fbcd089cbf1b23
Carolina Fernandez <carolina.fernandez@i2cat.net>
2014-12-17 17:58:03 +0100

    Making CBAS enabled by default and adding it to localsettings   > TODO: 1) integrate CBAS with OFVER so it starts on install/upgrade, 2) provide init script for CBAS

--- expedient/src/python/expedient/clearinghouse/defaultsettings/cbas.py
+++ expedient/src/python/expedient/clearinghouse/defaultsettings/cbas.py
@@ -1,10 +1,13 @@
-__author__ = 'umar.toseef'
+__author__ = "umar.toseef"
+
 """
 This file contains C-BAS configuration parameters
 """
 
-ENABLE_CBAS = False
-CBAS_DEBUG = False # In order to enable debugging, touch file: /var/log/apache2/expedient/clearinghouse/cbas.log and change ownership to 'www-data'
-CBAS_HOST_IP='127.0.0.1'
-CBAS_HOST_PORT=8008
-CBAS_HOST_NAME='cbas.eict.de'
+ENABLE_CBAS = True
+# In order to enable debugging, touch file: /var/log/apache2/expedient/clearinghouse/cbas.log and change ownership to "www-data"
+CBAS_DEBUG = False
+
+CBAS_HOST_IP = "127.0.0.1"
+CBAS_HOST_PORT = 8008
+CBAS_HOST_NAME = "cbas.eict.de"
