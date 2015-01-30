9a9ee021fabbd1bf6f7d2b4c053adac73224e5d5
Krzysztof Dombek <krzysztof.dombek@gmail.com>
2015-01-30 13:10:51 +0100

    PLugin changes

--- modules/resource/manager/stitching-entity/src/delegate/geni/v3/plugins/ryu_rest_of.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-def addSwitchingRule():
-    payload = {'key1': 'value1', 'key2': 'value2'}
-    r = requests.post("http://httpbin.org/postg", data=payload)
-    print r
-
-addSwitchingRule()
--- modules/resource/manager/stitching-entity/src/delegate/geni/v3/ryu_rest_of.py
+++ modules/resource/manager/stitching-entity/src/delegate/geni/v3/ryu_rest_of.py
@@ -1,2 +1,63 @@
+import requests
+
 def addSwitchingRule():
-	print "it works!"
+    print "start"
+    payload = {
+                "dpid":110533270894679,
+                "cookie":1,
+                "cookie_mask":1,
+                "table_id":0,
+                "idle_timeout":30,
+                "hard_timeout":30,
+                "priority":1,
+                "flags":1,  
+                 "match": {
+                             "dl_vlan":333,
+                             "in_port":12
+                          }, 
+                 "actions":[
+                                 {
+                                     "type":"SET_VLAN_VID",
+                                     "vlan_vid":555
+                                },
+                                 {
+                                     "type":"OUTPUT",
+                                     "port":13
+                                }
+                             ]
+                }
+    r = requests.post("http://127.0.0.1:8080/stats/flowentry/add", data=payload)
+    print r.status_code
+
+def deleteSwitchingRule():
+    print "start"
+    payload = {
+                "dpid":110533270894679,
+                "cookie":1,
+                "cookie_mask":1,
+                "table_id":0,
+                "idle_timeout":30,
+                "hard_timeout":30,
+                "priority":1,
+                "flags":1,  
+                 "match": {
+                             "dl_vlan":333,
+                             "in_port":12
+                          }, 
+                 "actions":[
+                                 {
+                                     "type":"SET_VLAN_VID",
+                                     "vlan_vid":555
+                                },
+                                 {
+                                     "type":"OUTPUT",
+                                     "port":13
+                                }
+                             ]
+                }
+    r = requests.post("http://127.0.0.1:8080/stats/flowentry/delete", data=payload)
+    print r.status_code
+
+if __name__ == '__main__':
+    # addSwitchingRule()
+    deleteSwitchingRule()
