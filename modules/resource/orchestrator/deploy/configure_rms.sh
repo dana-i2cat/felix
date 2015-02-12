8a8aa0193985351de50e4f32a2a7291d29f35ff8
Roberto Monno <r.monno@nextworks.it>
2015-02-12 14:13:16 +0100

    introduced a config script for mongoDB

---
+++ modules/resource/orchestrator/deploy/configure_rms.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+echo "Configuring Resource Orchestrator..."
+
+clean=${clean:-'no'}
+dummy=${dummy:-'no'}
+
+# Specify proper values here, if needed
+proto=${proto:-'https'}
+endpoint=${endpoint:-'xmlrpc/geni/3/'}
+user=${user:-""}
+pswd=${pswd:-""}
+
+# Configuring database with a dummy entry
+dbmanage_path="../src/admin/db"
+
+config_entry() {
+    echo -n "Please enter $1 IP address: "
+    read ip
+    echo -n "Please enter $1 PORT number: "
+    read port
+
+    python manage.py add_route_entry -t $1 -a $ip -p $port --protocol $proto --endpoint $endpoint
+}
+
+# Clean the environment
+if [[ $clean != "no" ]]; then
+    echo "Delete all tables..."
+    python $dbmanage_path/action_db.py --action "delete_all_tables"
+fi
+
+# Fill the database with dummy entries (if required)
+cd $dbmanage_path
+if [[ $dummy != "no" ]]; then
+    echo "Filling database with dummy entries..."
+    ./dummy_install.sh
+    echo "Filling database with dummy entries... Done"
+else
+    echo "Filling database with config entries..."
+    while true; do
+        echo
+        echo -n "Choose the RM to configure [c,sdn] (any other to quit): "
+        read x
+
+        if [[ x$x == x"c" ]]; then
+            config_entry "virtualisation"
+        elif [[ x$x == x"sdn" ]]; then
+            config_entry "sdn_networking"
+        else
+            break
+        fi
+    done
+    echo "Filling database with config entries... Done"
+fi
+cd - >/dev/null
+
+echo "Configuring Resource Orchestrator... Done"
--- modules/resource/orchestrator/deploy/install.sh
+++ modules/resource/orchestrator/deploy/install.sh
@@ -2,8 +2,6 @@
 
 echo "Installing Resource Orchestrator..."
 
-clean=${clean:-'no'}
-ip=${ip:-'127.0.0.1'}
 # Installing RO dependencies
 ./install_dependencies.sh
 
@@ -12,25 +10,6 @@ if [[ ! -L /etc/init.d/felix-ro ]]; then
   echo "Creating symlink..."
   sudo ln -s $PWD/bin/felix-ro.sh /etc/init.d/felix-ro
   echo "Creating symlink... Done"
-fi
-
-# Configuring database with a dummy entry
-dbmanage_path="../src/admin/db"
-
-if [[ $clean != "no" ]]; then
-    echo "Delete all tables..."
-    python $dbmanage_path/action_db.py --action "delete_all_tables"
-fi
-
-# Fill with data only when database is empty
-db_content=$(python $dbmanage_path/manage.py dump | wc -l)
-db_lines_to_show=$(($db_content-4))
-if [[ $(python $dbmanage_path/manage.py dump | tail -$db_lines_to_show) == "" ]]; then
-  echo "Filling database..."
-  cd $dbmanage_path
-  ./dummy_install.sh
-  cd - >/dev/null
-  echo "Filling database... Done"
 fi
 
 echo "Installing Resource Orchestrator... Done"
