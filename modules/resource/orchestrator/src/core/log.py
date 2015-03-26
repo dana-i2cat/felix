6bff153ff39c1e2db259f7c2cd0741ba7d131971
Roberto Monno <r.monno@nextworks.it>
2015-03-26 15:13:24 +0100

    a bit of color here

--- modules/resource/orchestrator/src/core/log.py
+++ modules/resource/orchestrator/src/core/log.py
@@ -1,33 +1,43 @@
 """
-This module provides logging facilities. More specifically, it provides a way to get to a (configured) python logger.
-Hence the interface of this logger is the same as the python one (so please direct all complaints regarding this to the python people). 
-In order to get such a logger instance, you could insert this code at the beginning of your module:
+This module provides logging facilities. More specifically,
+it provides a way to get to a (configured) python logger.
+Hence the interface of this logger is the same as the python one
+(so please direct all complaints regarding this to the python people).
+In order to get such a logger instance, you could insert this code
+at the beginning of your module:
     import amsoil.core.log
     logger=amsoil.core.log.getLogger('SOMENAME')
-whereby SOMENAME represents an optional prefix for the logging messages (e.g. 'xplugin' would yield messages like 'd.a.te [xplugin] message')
+whereby SOMENAME represents an optional prefix for the logging messages
+(e.g. 'xplugin' would yield messages like 'd.a.te [xplugin] message')
 
 Configuration
 Please see the config.py file in the root/src-folder.
 
 Rationale
 After long discussions logging is a core service.
-The main reason for having logging as a core service is that the pluginmanager should be
-able to log straight away (without loading a dedicated plugin). Also the administrator should have only one place to
-edit the log config (level, file, etc.). If you had a plugin for it, the config-plugin-service would not be available
+The main reason for having logging as a core service is that the pluginmanager
+should be able to log straight away (without loading a dedicated plugin).
+Also the administrator should have only one place to
+edit the log config (level, file, etc.). If you had a plugin for it,
+the config-plugin-service would not be available
 when the pluginmanager loads.
 """
 
-import logging, logging.handlers
+import logging
+import logging.handlers
 import os
+from colorlog import ColoredFormatter
 
 log_name = "resource-orchestrator"
-root_path = os.path.normpath(os.path.join(os.path.dirname(__file__), "../../log/"))
+root_path = os.path.normpath(os.path.join(os.path.dirname(__file__),
+                                          "../../log/"))
 log_file = os.path.join(root_path, "resource-orchestrator.log")
-log_format = "%(asctime)s [%(levelname)s] - %(message)s"
 log_level = logging.DEBUG
 
+
 def getLogger(prefix=None):
-    """Receive a python logger (logging.Logger) which has been configured by AMsoil."""
+    """Receive a python logger (logging.Logger) which has been
+    configured by AMsoil."""
     logger = logging.getLogger(log_name)
     if prefix:
         return PrefixAdapter(logger, prefix)
@@ -36,18 +46,26 @@ def getLogger(prefix=None):
 
 
 class PrefixAdapter(logging.LoggerAdapter):
-    """Internal class for wrapping logging messages. Do not use outside this module."""
+    """Internal class for wrapping logging messages.
+    Do not use outside this module."""
     def __init__(self, logger, prefix):
-        logging.LoggerAdapter.__init__(self, logger, { "prefix" : prefix })
+        logging.LoggerAdapter.__init__(
+            self, logger, {"prefix": prefix})
 
     def process(self, msg, kwargs):
         prefix = self.extra["prefix"]
         return ("[%s] %s" % (prefix, msg), kwargs)
-        
+
 # Initialization
-lhandle = logging.handlers.RotatingFileHandler(log_file, maxBytes = 1000000)
+lhandle = logging.handlers.RotatingFileHandler(log_file, maxBytes=1000000)
 lhandle.setLevel(log_level)
-lhandle.setFormatter(logging.Formatter(log_format))
+colormap = {'DEBUG': 'green', 'INFO': 'yellow', 'WARNING': 'blue',
+            'ERROR': 'red', 'CRITICAL': 'purple'}
+formatter = ColoredFormatter(
+    "%(log_color)s%(asctime)s [%(levelname)s] - %(message)s",
+    datefmt=None, reset=True, log_colors=colormap,
+    secondary_log_colors={}, style='%')
+lhandle.setFormatter(formatter)
 logger = getLogger()
 logger.addHandler(lhandle)
 logger.setLevel(log_level)
