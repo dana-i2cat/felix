b95ff5268a928dfa0692f6e24dfdaa398122d672
Carolina Fernandez <carolina.fernandez@i2cat.net>
2015-05-29 14:52:22 +0200

    SERM: cleaning up the RO folders and CRM, SDNRM parsers and formatters (reason: not used)

--- modules/resource/orchestrator/cert/copy_alice_credentials.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cp -p ~/.gcf/alice-cert.pem alice-cert.pem
-cp -p ~/.gcf/alice-key.pem alice-key.pem
--- modules/resource/orchestrator/src/server/flask/exceptions.py
+++
@@ -1,4 +0,0 @@
-from core.exception import CoreException
-
-class PrivateMethodCalledError(CoreException):
-    pass
