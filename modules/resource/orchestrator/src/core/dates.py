ecb0fbed4232545a42bcccc635b24623c6139cc4
Roberto Monno <r.monno@nextworks.it>
2015-06-16 16:30:56 +0200

    fixed import statement

--- modules/resource/orchestrator/src/core/dates.py
+++ modules/resource/orchestrator/src/core/dates.py
@@ -1,7 +1,6 @@
 from core import log
 from datetime import datetime
-#from dateutil import parser as dateparser
-import dateutil
+from dateutil import tz
 import re
 
 logger = log.getLogger("coreutils.dates")
@@ -9,6 +8,7 @@ logger = log.getLogger("coreutils.dates")
 # ---- Date utils ----
 
 RFC3339_FORMAT_STRING = "%Y-%m-%d %H:%M:%S.%fZ"
+
 
 def rfc3339_to_datetime(date):
     """
@@ -29,6 +29,7 @@ def rfc3339_to_datetime(date):
                  (type(formatted_date), formatted_date))
     return formatted_date
 
+
 def datetime_to_rfc3339(date):
     """
     Returns a string that is formatted according to RFC3339.
@@ -43,6 +44,7 @@ def datetime_to_rfc3339(date):
 #    return formatted_date
     return rfc3339format(date)
 
+
 def naiveUTC(dt):
     """
     Converts dt to a naive datetime in UTC.
@@ -52,10 +54,11 @@ def naiveUTC(dt):
     strip off timezone (make it "naive" in Python parlance)
     """
     if dt.tzinfo:
-        tz_utc = dateutil.tz.tzutc()
+        tz_utc = tz.tzutc()
         dt = dt.astimezone(tz_utc)
         dt = dt.replace(tzinfo=None)
     return dt
+
 
 def rfc3339format(dt):
     """
@@ -63,13 +66,15 @@ def rfc3339format(dt):
     """
     # Add UTC TZ, to have an RFC3339 compliant datetime, per the AM API
     naiveUTC(dt)
-    time_with_tz = dt.replace(tzinfo=dateutil.tz.tzutc())
+    time_with_tz = dt.replace(tzinfo=tz.tzutc())
     return time_with_tz.isoformat()
+
 
 def is_date(dt):
     if isinstance(dt, datetime):
         return True
     return False
+
 
 def is_rfc3339(dt):
     try:
@@ -81,10 +86,10 @@ def is_rfc3339(dt):
     except Exception:
         return False
 
+
 def is_date_or_rfc3339(dt):
     # Check if date is instance of "datetime.datetime"
     is_dt_a_date = is_date(dt)
     # Check if date complies with rfc3339
     is_dt_a_rfc3339 = is_rfc3339(dt)
     return is_dt_a_date or is_dt_a_rfc3339
-
