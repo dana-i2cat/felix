563716f40c594a9239bd21fd4338ba8f5d56fa35
Carolina Fernandez <carolina.fernandez@i2cat.net>
2015-10-30 15:02:17 +0000

    Removing dependencies to expedient (duplicating basic installation files)

--- expedient/src/python/expedient/clearinghouse/help/urls.py
+++
@@ -1,9 +0,0 @@
-"""
-@author: CarolinaFernandez
-"""
-
-from django.conf.urls.defaults import patterns, url
-
-urlpatterns = patterns('expedient.clearinghouse.help.views',
-    url(r'^', 'home', name="help_home"),
-)
