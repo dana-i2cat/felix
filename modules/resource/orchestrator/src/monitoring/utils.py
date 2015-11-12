4b5f843c7ccf069142ddc735aac00109aa43870c
Carolina Fernandez <carolina.fernandez@i2cat.net>
2015-11-12 18:52:54 +0100

    Monitoring (slice): removing repeated connections under abstract link

--- modules/resource/orchestrator/src/monitoring/utils.py
+++ modules/resource/orchestrator/src/monitoring/utils.py
@@ -20,6 +20,8 @@ class MonitoringUtils(object):
                     if node == "link":
                         interfaces_same_link = True
                         elem = root.xpath("//%s[@type='%s']//interface_ref[@client_id='%s']" % (node, node_type, node_urn))
+                        if node_type == None:
+                            elem = root.xpath("//%s//interface_ref[@client_id='%s']" % (node, node_urn))
                         for element in elements:
                             if element.getparent() == elem[0].getparent():
                                 interfaces_same_link &= True
@@ -31,9 +33,13 @@ class MonitoringUtils(object):
                         if node_type == "tn":
                             if domain is not None:
                                 domain = domain if "urn" in domain else "urn:publicid:IDN+ocf:" + domain
+                                if node_type == None:
+                                    elements = root.xpath("//topology[@name='%s']//%s[@id='%s']" % (domain, node, node_urn))
                                 elements = root.xpath("//topology[@name='%s']//%s[@type='%s'][@id='%s']" % (domain, node, node_type, node_urn))
                         else:
                             elements = root.xpath("//%s[@type='%s'][@id='%s']" % (node, node_type, node_urn))
+                            if node_type == None:
+                                elements = root.xpath("//%s[@id='%s']" % (node, node_urn))
             except Exception:
                 pass
             if len(elements) > 0:
