17998db7e208572b9bd0c2bdc44b1df44f3b3f11
Carolina Fernandez <carolina.fernandez@i2cat.net>
2015-11-27 16:23:09 +0100

    GUI: show M/RO peer information through REST (II)

--- modules/resource/orchestrator/src/server/flask/views.py
+++ modules/resource/orchestrator/src/server/flask/views.py
@@ -20,10 +20,11 @@ def peers():
     # Retrieve domain URN per peer
     for route in domain_routing:
         try:
-            route_info = current_app.mongo.get_info_peers({"_ref_peer": route["_id"]}).next()
-            route["urn"] = route_info["domain_urn"]
+            peer_urns = []
+            assoc_peers = current_app.mongo.get_info_peers({"_ref_peer": route["_id"]})
+            for r in range(0, assoc_peers.count()):
+                peer_urns.append(assoc_peers.next().get("domain_urn"))
+            route["urn"] = peer_urns
         except:
             pass
-    return render_template("peers.html",
-        db=current_app.db,
-        domain_routing=domain_routing)
+    return render_template("peers.html", db=current_app.db, domain_routing=domain_routing)
